264ee77dad9d0884d3093722ab5fa38cb9b5dfef
Mikhail Kazakov <miklelo@yandex.ru>
2018-03-26 01:18:31 +0700

    added notification, but only for one day. Next thing to do adding weekdays.

--- main/java/ru/miklelolyandex/officeexercises/SettingsActivity.java
+++ main/java/ru/miklelolyandex/officeexercises/SettingsActivity.java
@@ -1,5 +1,6 @@
 package ru.miklelolyandex.officeexercises;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
@@ -7,6 +8,10 @@ import android.preference.Preference;
 import android.preference.PreferenceManager;
 import android.preference.ListPreference;
 import android.preference.SwitchPreference;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -42,6 +47,34 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             bindPreferenceSummaryToValue(findPreference("exercise_duration"));
             bindPreferenceSummaryToValue(findPreference("interval_duration"));
             bindPreferenceSummaryToValue(findPreference("interval_timer"));
+
+            switchListener(findPreference("switch_notification"));
+            switchListener(findPreference("beginNotification"));
+            switchListener(findPreference("endNotification"));
+            switchListener(findPreference("interval_timer"));
+        }
+    }
+
+    private static void switchListener(Preference preference){
+
+        if (preference instanceof SwitchPreference) {
+
+            preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    SwitchPreference pref = (SwitchPreference) preference;
+                    turnOffOnNotificaton(preference, pref.isChecked());
+                    return false;
+                }
+            });
+        } else {
+            preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    turnOffOnNotificaton(preference, true);
+                    return false;
+                }
+            });
         }
     }
 
@@ -57,7 +90,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                     preference,
                     PreferenceManager
                             .getDefaultSharedPreferences(preference.getContext())
-                            .getBoolean(preference.getKey(),true));
+                            .getBoolean(preference.getKey(),false));
         } else {
             sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
                     PreferenceManager
@@ -90,7 +123,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 boolean logicValue = switchPreference.isEnabled();
 
                 // Set the summary to reflect the new value.
-                preference.setSummary(logicValue == false ? "Disabled" : "Enabled");
+                preference.setSummary(!logicValue ? "Disabled" : "Enabled");
 
             } else {
                 // For all other preferences, set the summary to the value's
@@ -100,5 +133,58 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             return true;
         }
     };
+
+    private static void changeNotificationSchedule(Preference preference, String whatHasChanged, String value){
+
+    }
+
+    private static void turnOffOnNotificaton(Preference preference, boolean notificationIsOn){
+
+        if (notificationIsOn) {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String beginTime = preferences.getString("beginNotification", "9:00");
+            String endTime = preferences.getString("endNotification", "17:00");
+            String intervalPref = preferences.getString("interval_timer", "60");
+            int beginHour = Integer.valueOf(beginTime.substring(0,2));
+            int beginMinute = Integer.valueOf(beginTime.substring(3));
+            int endHour = Integer.valueOf(endTime.substring(0,2));
+            int endMinute = Integer.valueOf(endTime.substring(3));
+            int interval = Integer.valueOf(intervalPref);
+
+            Calendar rightNow = Calendar.getInstance();
+            int currentHour = rightNow.get(Calendar.HOUR_OF_DAY);
+            int currentMinute = rightNow.get(Calendar.MINUTE);
+            if (currentMinute + interval > 60){
+                currentMinute = currentMinute + interval - 60;
+                currentHour++;
+            } else{
+                currentMinute += interval;
+            }
+            int currentTime = currentHour * 60 + currentMinute;
+            final int midnight = 24*60;
+            if (currentTime >= midnight){
+                currentTime -= midnight;
+                currentHour = 0;
+            }
+
+            int timeEnd = endHour*60 + endMinute;
+            int timeBegin = beginHour*60 + beginMinute;
+
+            if (currentTime < timeEnd && currentTime > timeBegin){
+                NotificationScheduler.setReminder(preference.getContext(), AlarmReceiver.class, currentHour, currentMinute);
+            } else if (currentTime < timeBegin || currentTime > timeEnd) {
+                if (beginMinute + interval > 60){
+                    beginMinute = beginMinute + interval - 60;
+                    beginHour++;
+                } else {
+                    beginMinute += interval;
+                }
+                NotificationScheduler.setReminder(preference.getContext(), AlarmReceiver.class, beginHour, beginMinute);
+            }
+
+        } else {
+            NotificationScheduler.cancelReminder(preference.getContext(), AlarmReceiver.class);
+        }
+    }
 
 }
